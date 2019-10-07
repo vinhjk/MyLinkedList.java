d85e4a34e8cef08e8d7c31fca97d773afe9a1896
NguyenVinh <nguyenquangvinh2518@gmail.com>
2019-10-07 08:50:20 +0700

    Linked List

---
+++ src/TestMyLinkedList.java
@@ -0,0 +1,12 @@
+public class TestMyLinkedList {
+    public static void main(String[] args) {
+        MyLinkedList ll = new MyLinkedList(9);
+        ll.addFirst(11);
+        ll.addFirst(12);
+        ll.addFirst(13);
+
+        ll.add(4,9);
+        ll.add(4,8);
+        ll.printList();
+    }
+}
